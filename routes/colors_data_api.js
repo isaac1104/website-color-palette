9514fa827f9d4cca6b3561026f6061725c2d27da
isaac1104 <isaac-kwon@hotmail.com>
2019-06-05 22:49:18 +0900

    send css color data from api/colors route

--- routes/colors_data_api.js
+++ routes/colors_data_api.js
@@ -1,5 +1,12 @@
+const cssColors = require('css-color-names');
+
 module.exports = app => {
-  app.get('/api/test', (req, res) => {
-    res.send('hi');
+  app.get('/api/colors', (req, res) => {
+    try {
+      const data = Object.entries(cssColors).map(color => ({ name: color[0], hex: color[1] }));
+      res.status(200).send(data);
+    } catch (e) {
+      res.status(400).send(e);
+    }
   });
 };
