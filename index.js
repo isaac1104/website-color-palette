b6f7784702e6994b7f9e7495d138e59d110bc4f9
isaac1104 <isaac-kwon@hotmail.com>
2019-06-05 19:23:24 +0900

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server on PORT: ${PORT}`);
+});
