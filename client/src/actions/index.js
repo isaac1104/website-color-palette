3c2b0dac3b8259ebf3baef54c9fc278b7e073bf0
isaac1104 <isaac-kwon@hotmail.com>
2019-06-05 22:57:31 +0900

    setup actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { FETCH_COLORS_DATA_REQUEST, FETCH_COLORS_DATA_SUCCESS, FETCH_COLORS_DATA_FAIL } from './types';
+
+const fetchColorsDataRequest = () => ({
+  type: FETCH_COLORS_DATA_REQUEST,
+  payload: true
+});
+
+const fetchColorsDataSuccess = data => ({
+  type: FETCH_COLORS_DATA_SUCCESS,
+  payload: data
+});
+
+const fetchColorsDataFail = error => ({
+  type: FETCH_COLORS_DATA_FAIL,
+  payload: error
+});
+
+export const fetchColorsData = () => async dispatch => {
+  dispatch(fetchColorsDataRequest());
+  try {
+    const request = await axios.get('/api/colors');
+    const { data } = request;
+    dispatch(fetchColorsDataSuccess(data));
+  } catch (e) {
+    dispatch(fetchColorsDataFail(error));
+  }
+};
