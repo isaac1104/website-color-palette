38554598222e589389fae57badefdc928059917f
isaac1104 <isaac-kwon@hotmail.com>
2019-06-06 15:55:18 +0900

    add sliding & fade in effect

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slide from 'react-reveal/Slide';
 import { Col, Row } from 'antd';
 import Palette from '../Palette/Palette';
 import Website from '../Website/Website';
@@ -6,14 +7,18 @@ import styles from './Home.module.css';
 
 const Home = () => {
   return (
-    <Row className={styles.Home}>
-      <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-        <Palette />
-      </Col>
-      <Col xs={18} sm={18} md={18} lg={18} xl={18}>
-        <Website />
-      </Col>
-    </Row>
+      <Row className={styles.Home}>
+        <Slide left>
+          <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+            <Palette />
+          </Col>
+        </Slide>
+        <Slide right>
+          <Col xs={18} sm={18} md={18} lg={18} xl={18}>
+            <Website />
+          </Col>
+        </Slide>
+      </Row>
   );
 };
 
