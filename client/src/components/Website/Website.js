3b93265cbb4caf28c0bbb8cc0e2830d14b86f00c
isaac1104 <isaac-kwon@hotmail.com>
2019-06-06 11:25:20 +0900

    refactor contents

--- client/src/components/Website/Website.js
+++ client/src/components/Website/Website.js
@@ -2,36 +2,53 @@ import React, { Component } from 'react';
 import { Button, Typography } from 'antd';
 import styles from './Website.module.css';
 import macbook from '../../assets/macbook_pro_15.png';
+import { HEADERS, TEXTS } from './WebsiteContents';
 
 const { Title } = Typography;
 
 class Website extends Component {
+  state = {
+    background: '#ffffff',
+    text: '#000000',
+    button: '#ffffff'
+  };
+
+  renderHeaders(section) {
+    return HEADERS[section].map(({ content, level }, index) => (
+      <Title
+        key={index}
+        level={level}
+        style={{ color: 'black' }}
+      >
+        {content}
+      </Title>
+    ));
+  }
+
+  renderTexts() {
+    return TEXTS.map(({ content }) => <Typography style={{ color: this.state.text }}>{content}</Typography>);
+  }
+
   render() {
     return (
       <div
         className={styles.WebsiteContainer}
+        style={{ backgroundColor: this.state.background }}
       >
         <div className={styles.WebsiteHeader}>
-          <Title level={4}>Macbook Pro</Title>
-          <Title level={2}>More power.</Title>
-          <Title level={2}>More performance.</Title>
-          <Title level={2}>More pro.</Title>
+          {this.renderHeaders('top')}
         </div>
         <div className={styles.WebsiteProduct}>
           <div>
-            <Title level={4}>Touch Bar and Touch ID</Title>
-            <Title level={4}>2.6GHz 6-Core Processor with Turbo Boost up to 4.5GHz</Title>
-            <Title level={4}>256GB Storage</Title>
-            <Title level={2}>$2,399.00</Title>
-            <Typography>2.6GHz 6-core 9th-generation Intel Core i7 processor</Typography>
-            <Typography>Turbo Boost up to 4.5GHz</Typography>
-            <Typography>Radeon Pro 555X with 4GB of GDDR5 memory</Typography>
-            <Typography>16GB 2400MHz DDR4 memory</Typography>
-            <Typography>256GB SSD storage</Typography>
-            <Typography>Retina display with True Tone</Typography>
-            <Typography>Touch Bar and Touch ID</Typography>
-            <Typography>Four Thunderbolt 3 ports</Typography>
-            <Button size='large' className={styles.BuyNowButton}>Buy Now!</Button>
+            {this.renderHeaders('bottom')}
+            {this.renderTexts()}
+            <Button
+              size='large'
+              className={styles.BuyNowButton}
+              style={{ backgroundColor: this.state.button }}
+            >
+              Buy Now!
+            </Button>
           </div>
           <img src={macbook} alt='Macbook Pro 15-inch' className={styles.ProductImage} />
         </div>
---
+++ client/src/components/Website/WebsiteContents.js
@@ -0,0 +1,25 @@
+export const HEADERS = {
+  top: [
+    { content: 'Macbook Pro', level: 4 },
+    { content: 'More power.', level: 2 },
+    { content: 'More performance.', level: 2 },
+    { content: 'More pro', level: 2 }
+  ],
+  bottom: [
+    { content: 'Touch Bar and Touch ID', level: 4 },
+    { content: '2.6GHz 6-Core Processor with Turbo Boost up to 4.5GHz', level: 4 },
+    { content: '256GB Storage', level: 4 },
+    { content: '$2,399.00', level: 2 },
+  ]
+};
+
+export const TEXTS = [
+  { content: '2.6GHz 6-core 9th-generation Intel Core i7 processor' },
+  { content: 'Turbo Boost up to 4.5GHz' },
+  { content: 'Radeon Pro 555X with 4GB of GDDR5 memory' },
+  { content: '16GB 2400MHz DDR4 memory' },
+  { content: '256GB SSD storage' },
+  { content: 'Retina display with True Tone' },
+  { content: 'Touch Bar and Touch ID' },
+  { content: 'Four Thunderbolt 3 ports' },
+];
