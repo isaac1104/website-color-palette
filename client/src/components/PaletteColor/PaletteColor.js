fb1380d43b20cacc719037d5f0e90fe1388815cf
isaac1104 <isaac-kwon@hotmail.com>
2019-06-06 13:03:00 +0900

    remove popover

--- client/src/components/PaletteColor/PaletteColor.js
+++ client/src/components/PaletteColor/PaletteColor.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import { Popover } from 'antd';
 import styles from './PaletteColor.module.css';
 
 const PaletteColor = ({ color, onClick }) => {
   const { hex, name } = color;
   return (
-    <Popover content={name}>
-      <div
-        className={styles.PaletteColor}
-        style={{ backgroundColor: hex }}
-        onClick={onClick}
-      />
-    </Popover>
+    <div
+      className={styles.PaletteColor}
+      style={{ backgroundColor: hex }}
+      onClick={onClick}
+    />
   );
 };
 
