cff6694098477a1e58c9e4cbe927302ab1e2d1e5
isaac1104 <isaac-kwon@hotmail.com>
2019-06-06 09:30:45 +0900

    refactor data

--- routes/colors_data_api.js
+++ routes/colors_data_api.js
@@ -3,7 +3,10 @@ const cssColors = require('css-color-names');
 module.exports = app => {
   app.get('/api/colors', (req, res) => {
     try {
-      const data = Object.entries(cssColors).map(color => ({ name: color[0], hex: color[1] }));
+      const data = Object.entries(cssColors).map(color => {
+        const [ name, hex ] = color;
+        return { name, hex };
+      });
       res.status(200).send(data);
     } catch (e) {
       res.status(400).send(e);
